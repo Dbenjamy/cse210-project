458b6fcf3cd877a128c280edd45278c784d84bbf
robbieswan <64027345+robbieswan@users.noreply.github.com>
2021-03-13 22:48:42 -0700

    Made updates to Constants

--- game/constants.py
+++ game/constants.py
@@ -5,20 +5,27 @@ DIRROOT = DIR.resolve().parent
 
 MAX_X = 800
 MAX_Y = 600
-NUM_BALLS = 5
 
-BALL_SPEED = 3
-BALL_Y = MAX_Y / 2
+#Previously BALL_SPEED
+DIRT_SPEED = 3
+DIRT_BOTTOM_SPEED = 3
+
+# Gives space between both Dirt Sprite Images so Car can
+# fit in between both and have road to drive on
+DIRT_Y = (MAX_Y / 2) + 40
+DIRT_BOTTOM_Y = (MAX_Y / 2) - 40
 
 CAR_X = 25
+# Would we want this to be... 
+# CAR_X = MAX_X / 2
 
 CAR_MOVE_SCALE = 10
 
-BRICK_WIDTH = 25
-BRICK_HEIGHT = 15
-BRICK_SPACE = 10
+# BRICK_WIDTH = 25
+# BRICK_HEIGHT = 15
+# BRICK_SPACE = 10
 
-BALLS_CAN_DIE = False
+# BALLS_CAN_DIE = False
 
 BALL_IMAGE = DIRROOT.joinpath("images/ball-0.png")
 
